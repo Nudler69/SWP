a3b6ad6809c445b17d09536fbc4a81bd23b5bac3
Nudler69 <nikola.barac@student.htldornbirn.at>
2024-01-15 09:22:28 +0100

    A7 HTML

--- A7 HTML - Eingabe Evaluation/file.js
+++ A7 HTML - Eingabe Evaluation/file.js
@@ -1,44 +1,54 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('#file').parsley({
         validators: {
-            filemaxsize: function() {
+            filemaxsize: function () {
                 return {
                     validate: function (val, max_megabytes, parsleyField) {
                         if (!Modernizr.fileapi) { return true; }
-                        
+
                         var $file_input = $(parsleyField.element);
                         if ($file_input.is(':not(input[type="file"])')) {
-                            console.log("Validation on max file size only works on file input types");
+                            console.log("Die Validierung der maximalen Dateigröße funktioniert nur für Datei-Eingabetypen.");
                             return true;
                         }
-                        
+
                         var max_bytes = max_megabytes * BYTES_PER_MEGABYTE, files = $file_input.get(0).files;
-                        
+
                         if (files.length == 0) {
-                            // No file, so valid. (Required check should ensure file is selected)
+                            // Keine Datei ausgewählt, daher gültig. (Die erforderliche Überprüfung sollte sicherstellen, dass eine Datei ausgewählt ist)
                             return true;
                         }
-                        
-                        return files.length == 1  && files[0].size <= max_bytes;
+
+                        var isTxtFile = true;
+                        for (var i = 0; i < files.length; i++) {
+                            var fileName = files[i].name;
+                            if (!fileName.toLowerCase().endsWith('.txt')) {
+                                isTxtFile = false;
+                                break;
+                            }
+                        }
+
+                        return isTxtFile && files.length == 1 && files[0].size <= max_bytes;
                     },
                     priority: 1
                 };
             }
         },
         messages: {
-            filemaxsize: "The file cannot be more than %s megabytes."
-        }
-        , excluded: 'input[type=hidden] :disabled'
+            filemaxsize: "Die Datei darf nicht mehr als 5 Megabyte groß sein.",
+            filetxt: "Nur TXT-Dateien sind erlaubt."
+        },
+        excluded: 'input[type=hidden] :disabled'
     });
-    
+
     /**
-		 * Extension to Modernizer for File API support
-		 */
-    window.Modernizr.addTest('fileapi', function() { return window.File && window.FileReader; });
-    
+     * Erweiterung zu Modernizer für die Unterstützung der File API
+     */
+    window.Modernizr.addTest('fileapi', function () { return window.File && window.FileReader; });
+
     window.BYTES_PER_MEGABYTE = 1048576;
-    
-    $('#file').on("submit", function(e) {
+
+    $('#file').on("submit", function (e) {
         e.preventDefault();
         $(this).parsley("validate");
     });
